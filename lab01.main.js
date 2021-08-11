028dac03589eaa05c9712ef00c99bbcc6f4bc903
mellecks <scott.mellecker@lumen.com>
2021-08-11 23:25:39 +0000

    Lab 2 part 1 of 2

--- lab01.main.js
+++ lab01.main.js
@@ -5,7 +5,7 @@
 */
 const IPCIDR = require('ip-cidr');
 
-/**  
+/**
  * Calculate and return he first host IP address from a CIDR subnet.
  * @param {string} cidrStr - The IPv4 subnet expressed
  *                in CIDR format.
@@ -49,38 +49,12 @@ function getFirstIpAddress(cidrStr, callback) {
    We will make several positve and negative tests.
 */
 
-function getIpv4MappedIpv6Address(ipv4) {
-  let ipv6Address = null; 
-  let ipv4Quads = ipv4.split('.');
-  let numIpv4Segments = ipv4Quads.length;
-  if (numIpv4Segments === 4) {
-    let validQuads = true;
-    for(let i=0; i < numIpv4Segments; i++) {
-      if( isNaN(Number(ipv4Quads[i]))  || ipv4Quads[i] < 0 || ipv4Quads[i] > 255 ) {
-        validQuads = false;
-      }
-    }
-    if (validQuads) {
-      ipv6address = "0:0:0:0:0:ffff:";
-      for(let i=0; i < numIpv4Segments; i++) {
-        let hexString = parseInt(ipv4Quads[i]).toString(16);
-        if(hexString.length % 2)
-          hexString = '0' + hexString;
-        if(i==1) {
-          ipv6address = ipv6Address + ':';
-        }  
-      }
-    }
-  }
-}  
-return ipv6address;
-
 function main() {
   // Creat an array of tests with both valid CIDR and invalid IP subnets.
   let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
   let sampleCidrslen = sampleCidrs.length;
-  let sampleIpv4s = ['172.16.10.1', '172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.256.1', '1.1.1.-1'];
-  let sampleIpv4slen = sampleIpv4s.length;
+
+  //Iterate over sampleCidrs and pass the element's value to getFirstIpAddress().
   for (var i = 0; i < sampleCidrslen; i++) {
     console.log(`\n--- Test Number ${i + 1} getFirstIpAddress(${sampleCidrs[i]}) ---`);
     // Call getFirstIpAddress and pass the test subnet and an anonymous callback function.
@@ -95,15 +69,8 @@ function main() {
       }
     });  
   }
-  for(let i =0; i < sampleIpv4slen; i++) {
-    let mappedaddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
-    if( mappedaddress ) {
-       console.log (` IPv4 ${sampleIpv4s[i]} mapped to IPv6 Address: ${mappedaddress}`);
-    } else {
-      console.error(` Problem converting IPv4 ${sampleIpv4s[i]} into a IPv6 address.`);  
-    }  
-  } 
 }
+
 /*
   Call main to run it.
 */
