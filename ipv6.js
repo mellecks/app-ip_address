47946906197f8ba2cf89120f191ce96b8f86071a
mellecks <scott.mellecker@lumen.com>
2021-08-13 14:36:06 +0000

    Lab 3

--- ipv6.js
+++ ipv6.js
@@ -1,3 +1,26 @@
+module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
+
+/*/**
+ * Calculates an IPv4-mapped IPv6 address.
+ * @param {string} ipv4 - An IPv4 address in dotted-quad format.
+ * @return {*} (ipv6Address) - An IPv6 address string or null if a run-time problem was detected.
+ */
+
+/*
+  This section is used to test function and log any errors.
+  We will make several positive and negative tests.
+*/*
+module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
+ * Calculates an IPv4-mapped IPv6 address.
+ * @param {string} ipv4 - An IPv4 address in dotted-quad format.
+ * @return {*} (ipv6Address) - An IPv6 address string or null if a run-time problem was detected.
+ */
+
+/*
+  This section is used to test function and log any errors.
+  We will make several positive and negative tests.
+*/
+
 function getIpv4MappedIpv6Address(ipv4) {
 
   // Initialize return argument
@@ -42,31 +65,3 @@ function getIpv4MappedIpv6Address(ipv4) {
   return ipv6Address;
 }
 module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
-
-/*
-  This section is used to test function and log any errors.
-  We will make several positive and negative tests.
-*/
-function main() {
-  // Create some test data for getFirstIpAddress(), both valid and invalid.
-  let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
-  let sampleCidrsLen = sampleCidrs.length;
-  // Create some test data for getIpv4MappedIpv6Address, both valid and invalid.
-  let sampleIpv4s = [ '172.16.10.1', '172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.256.1', '1.1.1.-1'];
-  let sampleIpv4sLen = sampleIpv4s.length;
-
-  // Iterate over sampleCidrs and pass the element's value to getFirstIpAddress().
-  for (let i = 0; i < sampleCidrsLen; i++) {
-    console.log(`\n--- Test Number ${i + 1} getFirstIpAddress(${sampleCidrs[i]}) ---`);
-    // Call getFirstIpAddress and pass the test subnet and an anonymous callback function.
-    // The callback is using the fat arrow operator: () => { }
-    getFirstIpAddress(sampleCidrs[i], (data, error) => {
-      // Now we are inside the callback function.
-      // Display the results on the console.
-      if (error) {
-        console.error(`  Error returned from GET request: ${error}`);
-      }
-      const { ipv4, ipv6} = data;
-      console.log(`  Response returned from GET request: {"ipv4":"${ipv4}","ipv6":"${ipv6}"}`);
-    });
-  }
